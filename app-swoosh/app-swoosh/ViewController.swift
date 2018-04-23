007eb02ed45e4a1f34d059ba41117e139cba4040
MoHapX <lavrenyk@gmail.com>
2018-04-24 01:05:06 +0300

    create a project & made a disign of the app fist page

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by MoHapX on 24.04.2018.
+//  Copyright © 2018 R'systems ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
