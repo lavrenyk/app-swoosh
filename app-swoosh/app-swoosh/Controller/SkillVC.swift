3b39348fd7aa6f5bf594ed5105877b458c9339c4
MoHapX <lavrenyk@gmail.com>
2018-04-24 22:00:24 +0300

    added seague functionality

---
+++ app-swoosh/app-swoosh/Controller/SkillVC.swift
@@ -0,0 +1,21 @@
+//
+//  SkillVC.swift
+//  app-swoosh
+//
+//  Created by MoHapX on 24.04.2018.
+//  Copyright © 2018 R'systems ltd. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
